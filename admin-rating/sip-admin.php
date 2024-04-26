903e08624c8f12048bd91ee6060fb98dfe675b9e
KboyVillahermosa <villahermosafrancisco6@gmail.com>
2024-04-26 18:38:46 +0800

    updated on april 26

--- admin-rating/sip-admin.php
+++ admin-rating/sip-admin.php
@@ -1,11 +1,12 @@
 <?php
-require 'config.php';
+require '../config.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['login_id'])) {
     header('Location: login.php');
     exit;
 }
+
 // Fetch all users from database
 $query_users = "SELECT * FROM `users`";
 $result_users = mysqli_query($db_connection, $query_users);
@@ -36,6 +37,11 @@ if(isset($_POST['delete_review_id'])) {
      $query_delete_colorsinpo = "DELETE FROM colorsinpo WHERE review_id = :review_id";
      $statement_colorsinpo = $connect->prepare($query_delete_colorsinpo);
      $statement_colorsinpo->execute(array(':review_id' => $delete_review_id));
+
+     // Delete from sip
+     $query_delete_sip = "DELETE FROM sip WHERE review_id = :review_id";
+     $statement_sip = $connect->prepare($query_delete_sip);
+     $statement_sip->execute(array(':review_id' => $delete_review_id));
 }
 
 $query_reviews = "SELECT * FROM review_tables ORDER BY review_id DESC";
@@ -46,6 +52,9 @@ $result_reviews_dou = $connect->query($query_reviews_dou, PDO::FETCH_ASSOC);
 
 $query_reviews_colorsinpo = "SELECT * FROM colorsinpo ORDER BY review_id DESC";
 $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FETCH_ASSOC);
+
+$query_reviews_sip = "SELECT * FROM sip ORDER BY review_id DESC";
+$result_reviews_sip = $connect->query($query_reviews_sip, PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -54,7 +63,7 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="style/admin.css">
+    <link rel="stylesheet" href="./admin-css/admin.css">
     <title>Document</title>
 </head>
 <body>
@@ -72,7 +81,7 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
          </button>
         <a href="https://flowbite.com" class="flex ms-2 md:me-24">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-          <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+          <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">TechTool Admin</span>
         </a>
       </div>
       <div class="flex items-center">
@@ -103,7 +112,7 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
                   <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
                 </li>
                 <li>
-                  <a href="index.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
                 </li>
               </ul>
             </div>
@@ -113,11 +122,12 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
   </div>
 </nav>
 
+
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="../admin-page.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
@@ -126,7 +136,7 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
             </a>
          </li>
          <li>
-            <a href="./admin-rating/adobe-admin.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="../admin-rating/adobe-admin.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                </svg>
@@ -134,7 +144,7 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
             </a>
          </li>
          <li>
-            <a href="./admin-rating/colorsinpo-admin.php " class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="../admin-rating/colorsinpo-admin.php " class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
                </svg>
@@ -142,19 +152,11 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
             </a>
          </li>
          <li>
-            <a href="./admin-rating/colorhunt-admin.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="../admin-rating/colorhunt-admin.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                </svg>
                <span class="flex-1 ms-3 whitespace-nowrap">Colorhunt Rating</span>
-            </a>
-         </li>
-         <li>
-            <a href="./admin-rating/sip-admin.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Sip</span>
             </a>
          </li>
       </ul>
@@ -165,10 +167,9 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
 
 
 <section>
-    <div class="user-header">
-        <div class="user-content">
+    <div class="rating-header">
+        <div class="rating-content">
         <div class="relative overflow-x-auto">
-<p>Total Users: <?php echo $total_users; ?></p>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -176,30 +177,45 @@ $result_reviews_colorsinpo = $connect->query($query_reviews_colorsinpo, PDO::FET
                     ID
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Name
+                   User Name
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Email
+                  User Review
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Profile Picture
+                    User Rating
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Date Rate
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
                 </th>
             </tr>
         </thead>
         <tbody>
-        <?php while ($row = mysqli_fetch_assoc($result_users)): ?>
+        <?php while ($row = $result_reviews_sip->fetch()): ?>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                <?php echo $row['google_id']; ?>
+                <?php echo $row['review_id']; ?>
                 </th>
                 <td class="px-6 py-4">
-                <?php echo $row['name']; ?>
+                <?php echo $row['user_name']; ?>
                 </td>
                 <td class="px-6 py-4">
-                <?php echo $row['email']; ?>
+                <?php echo $row['user_review']; ?>
                 </td>
                 <td class="px-6 py-4">
-                <img src="<?php echo $row['profile_image']; ?>" alt="<?php echo $row['name']; ?>" width="50">
+                <?php echo $row['user_rating']; ?>
+                </td>
+                <td class="px-6 py-4">
+                    <?php echo $row['datetime']; ?>
+                </td>
+                <td class="px-6 py-4">
+                <form method="post" onsubmit="return confirm('Are you sure you want to delete this review?');">
+                        <input type="hidden" name="delete_review_id" value="<?php echo $row['review_id']; ?>">
+                        <button type="submit" class="delete">Delete</button>
+                    </form>
                 </td>
             </tr>
             <?php endwhile; ?>
