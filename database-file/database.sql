6f98172479baffd33d73d74b5158000d07bafc59
KboyVillahermosa <villahermosafrancisco6@gmail.com>
2024-05-02 19:54:21 +0800

    updated may5

--- database-file/database.sql
+++ database-file/database.sql
@@ -1,112 +1,102 @@
-
-------- DATABASE NAME -------------
--- google_login
-
--- REVIEW RATINGS --
+-- Review Tables --
 
 CREATE TABLE `review_table` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
----- COLRO SINPO ------
 
 CREATE TABLE `colorsinpo` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
----- COLRO DOU ------
 
 CREATE TABLE `dou` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-------- h1ppy hues ------
 CREATE TABLE `happy_hues` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
----khorma----
-CREATE TABLE `happy_hues` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+CREATE TABLE `khorma` (
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
----LOL COLORS----
 CREATE TABLE `lolcolors` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
----SIP----
 CREATE TABLE `sip` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
----- TAILWIND COMPONENTS ---
 CREATE TABLE `tailwind_components` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
----- TAILWIND CHEAT ---
 CREATE TABLE `tailwind_cheat` (
-  review_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  user_rating int(1) NOT NULL,
-  user_review text NOT NULL,
-  datetime int(11) NOT NULL
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+-- Google Login Table --
 
-
--------------- GOOOGLE LOGIN TABLE -----------------------
 CREATE TABLE `users` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
- `google_id` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
- `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
- `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
- `profile_image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+ `google_id` varchar(150) NOT NULL,
+ `name` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `profile_image` text NOT NULL,
  PRIMARY KEY (`id`),
  UNIQUE KEY `google_id` (`google_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
+-- Admin Table --
 
------------------------- admin table ---------------------
-DROP TABLE IF EXISTS `adminlogin`;
 CREATE TABLE IF NOT EXISTS `adminlogin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
@@ -115,28 +105,23 @@ CREATE TABLE IF NOT EXISTS `adminlogin` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
----------------------------- insert into ---------------
-INSERT INTO `adminlogin` (`id`, `username`, `password`) VALUES (NULL, 'admin', 'admin'), (NULL, 'admin2', 'admin2');
+-- Insertion into Admin Table --
+
+INSERT INTO `adminlogin` (`id`, `username`, `password`) VALUES 
+(NULL, 'admin', 'admin'), 
+(NULL, 'admin2', 'admin2');
 
 
------------------------- admin with image ----------------------
-INSERT INTO `adminlogin` (`id`, `username`, `password`, `image`, `email`)
-VALUES
-(NULL, 'admin', 'admin', 'admin_image.jpg', 'villahermosafrancisco6@gmail.com'),
-(NULL, 'admin2', 'admin2', 'admin2_image.jpg', 'jomaralberastine90@gmail.com');
+-- User Preferences Table --
 
-
-
-
------- user prefs----
 CREATE TABLE `user_prefs` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    likes TEXT,
-    improvements TEXT,
-    tools_needed TEXT,
-    interests ENUM('Frontend', 'Backend', 'Full Stack', 'Data Science', 'Web Design'),
-    experience INT,
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+   `user_id` INT NOT NULL,
+    `likes` TEXT,
+    `improvements` TEXT,
+    `tools_needed` TEXT,
+    `interests` ENUM('Frontend', 'Backend', 'Full Stack', 'Data Science', 'Web Design'),
+    `experience` INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
